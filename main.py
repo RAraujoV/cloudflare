47b59294865e62a78d759eb22b4ddec6858cec9f
RAraujoV <marosaaraujo@gmail.com>
2023-10-20 12:50:38 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,28 @@
-from flet import *
+import flet as ft
+def main(page: ft.Page):
+    page.title = "Flet Hello"
+    page.theme_mode = ft.ThemeMode.LIGHT
+    page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
+    page.vertical_alignment = ft.MainAxisAlignment.CENTER
 
-def main(page:Page):
-    page.bgcolor="red"
-    page.add()
-    Column([
-    Text("Despliegue OK",size=30,weight="bold")
-    ]
-
+    dlg = ft.AlertDialog(
+        title=ft.Text("Hola")
     )
 
-flet.app(target=main)
+    def open_dlg(e):
+        page.dialog = dlg
+        dlg.open = True
+        page.update()
+
+    page.appbar = ft.AppBar(
+        title=ft.Text("Hello Word...")
+    )
+    page.add(
+        ft.Row(
+            [
+                ft.Text("Hello world"),
+                ft.ElevatedButton(text = "Show hello world",on_click=open_dlg)
+            ]
+        )      
+    )
+ft.app(target=main)
